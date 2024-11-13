4f11a3b09b8dd22cb15e56f6d6daca7f37d492db
mo8 <mo8codes@icloud.com>
2024-11-13 21:22:43 +0000

    tested working final

--- cc.py
+++ cc.py
@@ -1,7 +1,7 @@
 import shutil
 from pathlib import Path
 
-# Create new folder "./exported_compose_files" if it doesn't already exist
+# Create new folder "./export_cc" if it doesn't already exist
 new_folder = Path('../export_cc')
 new_folder.mkdir(parents=True, exist_ok=True)
 
@@ -13,18 +13,16 @@ for dir_path in current_directory.rglob('*'):
     if dir_path.is_dir():
         # Check if there is a docker-compose.yml or docker-compose.yaml file in the folder
         for compose_file in dir_path.glob('docker-compose.y*ml'):
-            if compose_file.is_file():
-                # Print to show that a docker-compose file was found
-                #print(f"Found file: {compose_file}")
+            if compose_file.is_file():    
+                # Make new folder in export_cc with the same name as the folder where the compose file is currently stored
+                destination_folder = new_folder / dir_path.relative_to(current_directory)
+                destination_folder.mkdir(parents=True, exist_ok=True)
                 
                 # Define the destination path for the copied file
-                destination = new_folder / dir_path / compose_file.name
-                # Make new folder 
-                (new_folder / dir_path).mkdir(parents=True, exist_ok=True)
+                destination = destination_folder / compose_file.name
+                
                 # Convert both paths to absolute paths to avoid issues with relative vs absolute paths
                 if compose_file.resolve() != destination.resolve():
                     # Copy the file to the new folder
                     shutil.copy(compose_file, destination)
                     print(f"Copied {compose_file} to {destination}")
-                else:
-                    pass #print(f"Skipped copying {compose_file}, as it is already in the destination.")
